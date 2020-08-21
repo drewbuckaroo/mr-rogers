e26cc231dec7616d902e745fd3af3914190858a0
drewbuckaroo <drewmlockhart@gmail.com>
2020-08-21 15:10:10 -0700

    I am still re-reading the curriculum and watching videos trying to understand this weeks concepts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,8 @@
 function mrRogers(count) {
-  let numList = ["1", "2", "3"];
-  let roboList = ["Beep!", "Boop!", "Won't you be my neighbor?"];
-  let numList = roboList;
-  for (let i = 0; i < numList.length; i++){
-  while (count >= robolist[i]) 
-    result.push (numList[i]);
-    count === roboList[i];
-  }
-}
+  const numList = [1, 2, 3];
+  const roboList = ["Beep!", "Boop!", "Won't you be my neighbor?"];
+
+
 
 
 $(document).ready(function(){
