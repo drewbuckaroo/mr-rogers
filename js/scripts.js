08963dcb64d0ae53dd81dafe6867b78720d85a50
drewbuckaroo <drewmlockhart@gmail.com>
2020-08-21 16:11:43 -0700

    still struggling to understand for loops with if statements, still reading and watching.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-function mrRogers(count) {
-  const numList = [1, 2, 3];
-  const roboList = ["Beep!", "Boop!", "Won't you be my neighbor?"];
+const array = [1, 2, 3];
+let newArray = [];
+for (let index = 0; index < array.length; index++) {
+    newArray.push(array[index]);
+}
+    if (i = 3; "Won't you be my neighbor?")
+    console.log(i)
+
+  //const roboList = ["Beep!", "Boop!", "Won't you be my neighbor?"];
 
 
 
 
-$(document).ready(function(){
-    $("#").submit(function(event) {
-        event.preventDefault();
-
-    });
-});
