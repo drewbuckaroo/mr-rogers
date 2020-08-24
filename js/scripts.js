9673f88267f62139cec6d1d789cd7429459e6e78
drewbuckaroo <drewmlockhart@gmail.com>
2020-08-23 19:42:38 -0700

    Third console log test.

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,11 @@ function talkRobo(input){
   for (let i=0; i < i.length; i++) {
     if (index.toString().includes("3")) {
       roboArray.push("Won't you be my neighbor?");
-    } else if (index.toString().includes("2")){
-      console.log("Boop!");
+    } else if (index.toString().includes("2")) {
+      roboArray.push("Boop!");
+    } else if (index.toString().includes("1")) {
+        console.log("Beep!");
+      
     } else {}
 
   }
